68221207db62a82cf955d4a4e4492ed9a505407c
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-02-12 09:43:57 +0700

    Config MVC project

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import express, {Express, Request, Response} from "express";
+import express, {Express} from "express";
 import * as database from "./config/database";
 import env from "dotenv";
 
-import Topic from "./models/topic.model";
+import clientRoutes from "./routes/client/index.route";
 
 env.config();
 
@@ -14,15 +14,7 @@ app.set("view engine", "pug");
 
 database.connect();
 
-app.get("/topics", async (req: Request, res: Response) => {
-    const topics = await Topic.find({
-        deleted: false
-    })
-    console.log(topics);
-    res.render("client/pages/topics/index", {
-        pageTitle: "Trang chủ đề bài hát"
-    });
-})
+clientRoutes(app);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
