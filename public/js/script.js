65e10c49876b489d94c9ddcc2d5fe9f50bb56705
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-02-13 15:51:38 +0700

    Use aplayer and Create Detail song page

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,28 @@
-console.log("ok");
+//Aplayer
+const aplayer = document.querySelector('#aplayer');
+if(aplayer){
+    const dataSong = JSON.parse(aplayer.getAttribute("data-song"));
+    // dataSong = JSON.parse(dataSong)
+    const dataSinger = JSON.parse(aplayer.getAttribute("data-singer"));
+    // dataSinger = JSON.parse(dataSinger)
+    const ap = new APlayer({
+        container: aplayer,
+        audio: [{
+            name: dataSong.title,
+            artist: dataSinger.fullName,
+            url: dataSong.audio,
+            cover: dataSong.avatar
+        }],
+        autoplay: true
+    });
+
+    const avatar = document.querySelector(".singer-detail .inner-avatar");
+    
+    ap.on('pause', function () {
+        avatar.style.animationPlayState = "paused";
+    });
+
+    ap.on('play', function () {
+        avatar.style.animationPlayState = "running";
+    });
+}
