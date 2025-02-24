314735631a5f772aace62e95fb1dd0a4e13c1078
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-02-24 09:03:16 +0700

    Upload TinyMCE's image to cloudinary

--- public/admin/js/tinymce-config.js
+++ public/admin/js/tinymce-config.js
@@ -1,29 +1,31 @@
 tinymce.init({
     selector: 'textarea.textarea-mce',
+    images_upload_url: '/admin/upload',
+    automatic_uploads: true,
     plugins: 'lists link image table code help wordcount',
-    file_picker_callback: (cb, value, meta) => {
-      const input = document.createElement('input')
-      input.setAttribute('type', 'file')
-      input.setAttribute('accept', 'image/*')
+    // file_picker_callback: (cb, value, meta) => {
+    //   const input = document.createElement('input')
+    //   input.setAttribute('type', 'file')
+    //   input.setAttribute('accept', 'image/*')
   
-      input.addEventListener('change', (e) => {
-        const file = e.target.files[0]
+    //   input.addEventListener('change', (e) => {
+    //     const file = e.target.files[0]
   
-        const reader = new FileReader()
-        reader.addEventListener('load', () => {
-          const id = 'blobid' + (new Date()).getTime()
-          const blobCache = tinymce.activeEditor.editorUpload.blobCache
-          const base64 = reader.result.split(',')[1]
-          const blobInfo = blobCache.create(id, file, base64)
-          blobCache.add(blobInfo)
+    //     const reader = new FileReader()
+    //     reader.addEventListener('load', () => {
+    //       const id = 'blobid' + (new Date()).getTime()
+    //       const blobCache = tinymce.activeEditor.editorUpload.blobCache
+    //       const base64 = reader.result.split(',')[1]
+    //       const blobInfo = blobCache.create(id, file, base64)
+    //       blobCache.add(blobInfo)
   
-          /* call the callback and populate the Title field with the file name */
-          cb(blobInfo.blobUri(), {
-            title: file.name
-          })
-        })
-        reader.readAsDataURL(file);
-      })
-      input.click();
-    }
+    //       /* call the callback and populate the Title field with the file name */
+    //       cb(blobInfo.blobUri(), {
+    //         title: file.name
+    //       })
+    //     })
+    //     reader.readAsDataURL(file);
+    //   })
+    //   input.click();
+    // }
   })
---
+++ routes/admin/upload.route.ts
@@ -0,0 +1,16 @@
+import {Router} from "express";
+import multer from "multer";
+const router: Router = Router();
+
+import * as controller from "../../controllers/admin/upload.controller";
+import * as uploadCloud from "../../middlewares/uploadCloud.middleware";
+
+const upload = multer();
+
+router.post(
+    "/", 
+    upload.single("file"), 
+    uploadCloud.uploadSingle,
+    controller.index);
+
+export const uploadRoutes: Router = router;
