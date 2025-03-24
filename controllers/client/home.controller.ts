f24f1c037d981eaee5152f090d9cabc6b3a3e447
ledaoquyen2004@gmail.com <ledaoquyen2004@gmail.com>
2025-03-24 23:42:07 +0700

    Home page

--- controllers/client/home.controller.ts
+++ controllers/client/home.controller.ts
@@ -1,8 +1,24 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
+import Topic from "../../models/topic.model";
+import Song from "../../models/song.model";
 
 //[GET] /
 export const index = async (req: Request, res: Response) => {
+    const topics = await Topic.find({
+        status: "active",
+        deleted: false
+    }).sort({ createdAt: -1 })
+        .limit(3);
+
+    const songs = await Song.find({
+        status: "active",
+        deleted: false
+    }).sort({ createdAt: -1 })
+    .limit(4);
+
     res.render("client/pages/home/index", {
-        pageTitle: "Trang chủ",
+        pageTitle: "Chủ đề hot",
+        topics: topics,
+        songs: songs
     });
 }
--- controllers/client/topic.controller.ts
+++ controllers/client/topic.controller.ts
@@ -5,7 +5,7 @@ import Topic from "../../models/topic.model";
 export const topics = async (req: Request, res: Response) => {
     const topics = await Topic.find({
         deleted: false
-    })
+    }).sort({createdAt: -1}); //Sort latest topic
     
     res.render("client/pages/topics/index", {
         pageTitle: "Chủ đề bài hát",
